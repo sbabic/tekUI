fa883b6e294b5d915a12a8ebf1dd5910d09ba89f
Timm S. Mueller <tmueller@schulze-mueller.de>
2014-02-12 16:53:55 +0100

    Input: another fix to setting cursor mode on focus

--- tek/ui/class/input.lua
+++ tek/ui/class/input.lua
@@ -49,7 +49,7 @@ local tostring = tostring
 local type = type
 
 module("tek.ui.class.input", tek.ui.class.scrollgroup)
-_VERSION = "Input 4.1"
+_VERSION = "Input 4.2"
 local Input = _M
 ScrollGroup:newClass(Input)
 
@@ -396,9 +396,7 @@ function Input:onActivate()
 end
 
 function Input:onFocus()
-	if self.Focus then
-		self:setCursorMode(self.Focus and "active" or "hidden")
-	end
+	self:setCursorMode(self.Focus and "active" or "hidden")
 	self.EditInput:setValue("Focus", self.Focus)
 end
 
